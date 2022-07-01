ef0fb12470723d15437ce6868bfaefe753648ba8
mohirwanh <mohirwanh@gmail.com>
2022-07-01 14:36:36 +0700

    day2

---
+++ java-spring-boot-day2/src/main/java/com/solusione/day2/model/request/UserRequest.java
@@ -0,0 +1,13 @@
+package com.solusione.day2.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    private String email;
+    private String name;
+}
---
+++ java-spring-boot-day2/src/main/java/com/solusione/day2/model/response/UserResponse.java
@@ -0,0 +1,15 @@
+package com.solusione.day2.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse implements Serializable {
+    private String email;
+    private String name;
+}
