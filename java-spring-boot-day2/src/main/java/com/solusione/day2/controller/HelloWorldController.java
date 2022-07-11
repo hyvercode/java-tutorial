5f0992be70fa1efb3138c1770ca49b6e81c54154
mohirwanh <mohirwanh@gmail.com>
2022-07-11 11:11:48 +0700

    unit testing

--- java-spring-boot-day2/src/main/java/com/solusione/day2/controller/HelloWorldController.java
+++ java-spring-boot-day2/src/main/java/com/solusione/day2/controller/HelloWorldController.java
@@ -2,7 +2,6 @@ package com.solusione.day2.controller;
 
 import com.solusione.day2.model.request.UserRequest;
 import com.solusione.day2.model.response.UserResponse;
-import com.solusione.day2.service.RabbitMQSenderService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,13 +11,8 @@ import java.util.List;
 @RequestMapping("/api")
 public class HelloWorldController {
 
-//    RabbitMQSenderService  rabbitMQSenderService;
-
     private ArrayList<UserResponse> userResponses = new ArrayList<>();
 
-//    public HelloWorldController(RabbitMQSenderService rabbitMQSenderService) {
-//        this.rabbitMQSenderService = rabbitMQSenderService;
-//    }
 
     @GetMapping("/hello")
     public String getHello() {
@@ -48,7 +42,6 @@ public class HelloWorldController {
 
     @PostMapping("/users")
     public List<UserResponse> postUsers(@RequestBody UserRequest userRequest) {
-//        rabbitMQSenderService.send(userRequest);
         userResponses.add(new UserResponse(userRequest.getEmail(), userRequest.getName()));
         return userResponses;
     }
