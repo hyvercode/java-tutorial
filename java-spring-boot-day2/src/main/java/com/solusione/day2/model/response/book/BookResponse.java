fd618a2d90d634bf4117caca0a8a5ab5e90de6da
mohirwanh <mohirwanh@gmail.com>
2022-07-13 16:37:43 +0700

    orders service

--- java-spring-boot-day2/src/main/java/com/solusione/day2/model/response/book/BookResponse.java
+++ java-spring-boot-day2/src/main/java/com/solusione/day2/model/response/book/BookResponse.java
@@ -6,9 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
