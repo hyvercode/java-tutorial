a27480a0f0036b9b8edd9e6b64298850a679d575
mohirwanh <mohirwanh@gmail.com>
2022-07-17 23:52:22 +0700

    starter

--- java-spring-boot-day2/src/main/java/com/solusione/day2/Day2Application.java
+++ java-spring-boot-day2/src/main/java/com/solusione/day2/Day2Application.java
@@ -1,9 +1,14 @@
 package com.solusione.day2;
 
+import com.hyvercode.starter.annotations.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableSwagger
+@EnableConfigurationProperties(SecurityProperties.class)
 public class Day2Application{
 	public static void main(String[] args) {
 		SpringApplication.run(Day2Application.class, args);
